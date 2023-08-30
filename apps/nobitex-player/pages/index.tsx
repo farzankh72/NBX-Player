0cd51aa575e1344f6d49f5a4f237b21c636d3f06
Farzan khamse <43580634+farzankh72@users.noreply.github.com>
2023-08-30 14:15:49 +0330

    Update index.tsx

--- apps/nobitex-player/pages/index.tsx
+++ apps/nobitex-player/pages/index.tsx
@@ -1,31 +1,33 @@
 import VideoProvider from '../../../libs/nbxplayer/src/lib/VideoProvider'
-import {Container, Divider} from "@mui/material";
+import { Container, Divider } from '@mui/material'
 
-export function Index() {
-  <Container>
-    <VideoProvider
-      poster={'https://nobitex.ir/academy/api/file/download/ZtdNUgGvuyfuBoGuwEvi'}
-      videoData={'https://nobitex.ir/academy/api/file/download/UjYJHOZYPKnuzopPluZe'}
-    />
-    <Divider/>
-    <VideoProvider
-      poster={'https://nobitex.ir/academy/api/file/download/ZtdNUgGvuyfuBoGuwEvi'}
-      videoData={[
-        {
-          url: 'https://nobitex.ir/academy/api/file/download/UjYJHOZYPKnuzopPluZe',
-          quality: 'SD',
-        },
-        {
-          url: 'https://nobitex.ir/academy/api/file/download/YvTqNcoLiydduRCwHizM',
-          quality: 'HD',
-        },
-        {
-          url: 'https://nobitex.ir/academy/api/file/download/SDyKdGLPYwuJZLFDYBoD',
-          quality: 'FHD',
-        },
-      ]}
-    />
-  </Container>
+export const Index = () => {
+  return (
+    <Container>
+      <VideoProvider
+        poster={'https://nobitex.ir/academy/api/file/download/ZtdNUgGvuyfuBoGuwEvi'}
+        videoData={'https://nobitex.ir/academy/api/file/download/UjYJHOZYPKnuzopPluZe'}
+      />
+      <Divider />
+      <VideoProvider
+        poster={'https://nobitex.ir/academy/api/file/download/ZtdNUgGvuyfuBoGuwEvi'}
+        videoData={[
+          {
+            url: 'https://nobitex.ir/academy/api/file/download/UjYJHOZYPKnuzopPluZe',
+            quality: 'SD',
+          },
+          {
+            url: 'https://nobitex.ir/academy/api/file/download/YvTqNcoLiydduRCwHizM',
+            quality: 'HD',
+          },
+          {
+            url: 'https://nobitex.ir/academy/api/file/download/SDyKdGLPYwuJZLFDYBoD',
+            quality: 'FHD',
+          },
+        ]}
+      />
+    </Container>
+  )
 }
 
-export default Index;
+export default Index
